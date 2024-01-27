51105f00eba9e631728b73fa9180a5894a366295
prichith cp <prichith@gmail.com>
2024-01-27 20:54:27 +0530

    js started

---
+++ script.js
@@ -0,0 +1,33 @@
+// Main Page
+    function hellopage(){
+        window.location.href = "sayhello.html";
+    }
+    function conversationPage(){
+        window.location.href = "conversation.html";
+    }
+    function projectPage(){
+        window.location.href = "project.html";
+    }
+    function linkedin(){
+        window.location.href = "https://www.linkedin.com/in/prichith-c-p-3047a7220/";
+    }
+    function github(){
+        window.location.href = "https://github.com/prichith";
+    }
+    function instagram(){
+        window.location.href = "https://www.instagram.com/prichithcp/?hl=en";
+    }
+    function facebook(){
+        window.location.href = "https://www.facebook.com/prichith.cp.50";
+    }
+// End Main Page
+// Hello page
+    function returnback(){
+        window.location.href ="index.html";
+    }
+    function helloFormClear(){
+        document.getElementById("name").value = ""
+        document.getElementById("email").value = ""
+        document.getElementById("message").value = ""
+    }
+// END Hello page
