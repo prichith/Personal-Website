f6c9211cbd9b0cbdcea8b3579305edd9afe71ba2
prichith cp <prichith@gmail.com>
2024-06-23 22:07:56 +0530

    responsive work

--- script.js
+++ script.js
@@ -1,84 +1,82 @@
 // Main Page
-    function hellopage(){
-        window.location.href = "sayhello.html";
-    }
-    function conversationPage(){
-        window.location.href = "conversation.html";
-    }
-    function projectPage(){
-        window.location.href = "project.html";
-    }
-    function linkedin(){
-        window.location.href = "https://www.linkedin.com/in/prichith-c-p-3047a7220/";
-    }
-    function github(){
-        window.location.href = "https://github.com/prichith";
-    }
-    function instagram(){
-        window.location.href = "https://www.instagram.com/prichithcp/?hl=en";
-    }
-    function facebook(){
-        window.location.href = "https://www.facebook.com/prichith.cp.50";
-    }
-    // My Project Section
-        // function Pro1Hover(number){
-        //     `document.getElementById("pro${number}").style.display="block"`
-        // }
-        function Pro1Hover(){
-            document.getElementById("pro1").style.display="block"
-            document.getElementById("pro1").style.transition="ease 4000ms"
-        }
-        function Pro2Hover(){
-            document.getElementById("pro2").style.display="block"
-        }
-        function Pro3Hover(){
-            document.getElementById("pro3").style.display="block"
-        }
-        function hideBtn1(){
-            document.getElementById("pro1").style.display="none"
-        }
-        function hideBtn2(){
-            document.getElementById("pro2").style.display="none"
-        }
-        function hideBtn3(){
-            document.getElementById("pro3").style.display="none"
-        }
-        function responsive(){
-            window.location.href="https://prichith.github.io/ResponsiveWebsite_morethan5BreakPoints/"
-        }
-        function zomato(){
-            window.location.href="https://prichith.github.io/ZomatoCloneWebsite/"
-        }
-        function visitWebsite3(){
-            window.location.href="https://prichith.github.io/ResponsiveWebsite_morethan5BreakPoints/"
-        }
-    // END My Project Section
+function hellopage() {
+  window.location.href = "sayhello.html";
+}
+function conversationPage() {
+  window.location.href = "conversation.html";
+}
+function projectPage() {
+  window.location.href = "project.html";
+}
+function linkedin() {
+  window.location.href = "https://www.linkedin.com/in/prichith-c-p-3047a7220/";
+}
+function github() {
+  window.location.href = "https://github.com/prichith";
+}
+function instagram() {
+  window.location.href = "https://www.instagram.com/prichithcp/?hl=en";
+}
+function facebook() {
+  window.location.href = "https://www.facebook.com/prichith.cp.50";
+}
+
+function Pro1Hover() {
+  document.getElementById("pro1").style.display = "block";
+  document.getElementById("pro1").style.transition = "ease 4000ms";
+}
+function Pro2Hover() {
+  document.getElementById("pro2").style.display = "block";
+}
+function Pro3Hover() {
+  document.getElementById("pro3").style.display = "block";
+}
+function hideBtn1() {
+  document.getElementById("pro1").style.display = "none";
+}
+function hideBtn2() {
+  document.getElementById("pro2").style.display = "none";
+}
+function hideBtn3() {
+  document.getElementById("pro3").style.display = "none";
+}
+function responsive() {
+  window.location.href = "https://prichith.github.io/Responsive-Website/";
+}
+function zomato() {
+  window.location.href = "https://prichith.github.io/ZomatoCloneWebsite/";
+}
+function visitWebsite3() {
+  window.location.href =
+    "https://prichith.github.io/ResponsiveWebsite_morethan5BreakPoints/";
+}
+// END My Project Section
 // End Main Page
 // Hello page
-    function returnback(){
-        window.location.href ="index.html";
-    }
-    function helloFormClear(){
-        document.getElementById("name").value = ""
-        document.getElementById("email").value = ""
-        document.getElementById("helloMessage").value = ""
-    }
+function returnback() {
+  window.location.href = "index.html";
+}
+function helloFormClear() {
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("helloMessage").value = "";
+}
 // END Hello page
 // Conversation Form
-    function clearConversationForm(){
-        document.getElementById("name").value = ""
-        document.getElementById("email").value = ""
-        document.getElementById("project").value = ""
-        document.getElementById("interest").value = ""
-        document.getElementById("convDetails").value = ""
-    }
+function clearConversationForm() {
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("project").value = "";
+  document.getElementById("interest").value = "";
+  document.getElementById("convDetails").value = "";
+}
 // END Conversation Form
 // Project Page
-    function projectFormClear(){
-        document.getElementById("name").value = ""
-        document.getElementById("email").value = ""
-        document.getElementById("projectType").value = ""
-        document.getElementById("budget").value = ""
-        document.getElementById("projectDetails").value = ""
-    }
+function projectFormClear() {
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("projectType").value = "";
+  document.getElementById("budget").value = "";
+  document.getElementById("projectDetails").value = "";
+}
 // END Project Form
